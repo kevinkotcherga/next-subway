a63dd80218d8541c41d0e52c3f76ac571b859ee4
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-05-24 14:37:20 +0200

    feat(Home) : ajout de styles scss à la page Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import './home.scss';
 
 const Home = () => {
   return (
     <div className='home'>
-      <div className='container'>
-        <form>
-          <select>test</select>
-          <select>test</select>
-        </form>
-        <div className='results'>
-          <p>Résultats</p>
+      <div className='mainContainer'>
+        <div className='container'>
+          <form>
+            <select>test</select>
+            <select>test</select>
+          </form>
+          <div className='results'>
+            <p>Résultats</p>
+          </div>
         </div>
       </div>
     </div>
