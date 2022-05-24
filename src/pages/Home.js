238e1e1cb8c4bb7c0dddf39643a7b7f0fd6fe4ae
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-05-24 14:45:26 +0200

    feat(Home) : récupération des numéros de stations de métro avec l'API

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,42 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import './home.scss';
 
 const Home = () => {
-  return (
-    <div className='home'>
-      <div className='mainContainer'>
-        <div className='container'>
-          <form>
-            <select>test</select>
-            <select>test</select>
-          </form>
-          <div className='results'>
-            <p>Résultats</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  const [subwayStationNumber, setSubwayStationNumber] = useState([]);
+
+	// Récupération du numéro des stations
+  useEffect(() => {
+		const getSubwayStationNumbers = async () => {
+			try {
+				const { data } = await axios.get(
+					'https://api-ratp.pierre-grimaud.fr/v4/lines/metros',
+				);
+				setSubwayStationNumber(data.result.metros);
+			} catch (err) {
+				console.log(err);
+			}
+		};
+		getSubwayStationNumbers();
+	}, []);
+
+  console.log(subwayStationNumber);
+
+	return (
+		<div className="home">
+			<div className="mainContainer">
+				<div className="container">
+					<form>
+						<select>test</select>
+						<select>test</select>
+					</form>
+					<div className="results">
+						<p>Résultats</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Home;
